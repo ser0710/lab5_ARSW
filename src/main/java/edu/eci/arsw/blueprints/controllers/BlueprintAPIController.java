84b66372f8abf4e5be81a73b8a84936481512734
judibec <juanchopolo6954@gmail.com>
2022-09-12 18:47:33 -0500

    funciona localhost

--- src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
+++ src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
@@ -10,6 +10,7 @@ import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import edu.eci.arsw.blueprints.model.Blueprint;
 import edu.eci.arsw.blueprints.persistence.BlueprintNotFoundException;
 import edu.eci.arsw.blueprints.persistence.BlueprintsPersistence;
 import edu.eci.arsw.blueprints.services.BlueprintsServices;
@@ -26,18 +27,18 @@ import org.springframework.web.bind.annotation.RestController;
  * @author hcadavid
  */
 @RestController
-@RequestMapping(value = "/model/Blueprint")
-@Service
+@RequestMapping(value = "/blueprint")
+
 public class BlueprintAPIController {
 
     @Autowired
-    BlueprintsServices bps=null;
+    BlueprintsServices bps;
     @RequestMapping(method = RequestMethod.GET)
 
-    public ResponseEntity<?> getRecurso(){
+    public ResponseEntity<?> getBlueprint(){
         try{
-
-            return new ResponseEntity<>(bps.getAllBlueprints(), HttpStatus.ACCEPTED);
+            Set<Blueprint> data = bps.getAllBlueprints();
+            return new ResponseEntity<>(data, HttpStatus.ACCEPTED);
         } catch (BlueprintNotFoundException ex) {
             Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, ex);
             return new ResponseEntity<>("Error",HttpStatus.NOT_FOUND);
